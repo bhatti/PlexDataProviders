eb1ca23a231868533ff79e857ed71d6b54c541fa
Shahzad Bhatti <shahzad.bhatti@aperturegroup.com>
2016-01-31 21:34:01 -0800

    added query response

---
+++ dp-sample/src/main/java/com/plexobject/dp/sample/domain/QueryResponse.java
@@ -0,0 +1,15 @@
+package com.plexobject.dp.sample.domain;
+
+import com.plexobject.dp.domain.DataResponse;
+
+public class QueryResponse {
+    private DataResponse queryResponse;
+
+    public DataResponse getQueryResponse() {
+        return queryResponse;
+    }
+
+    public void setQueryResponse(DataResponse queryResponse) {
+        this.queryResponse = queryResponse;
+    }
+}
